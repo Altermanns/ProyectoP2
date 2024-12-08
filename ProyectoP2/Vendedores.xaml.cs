bd9ead65c90249cd5edfeb0d9b85e55a0524bc23
Altermanns <isaac.277.trujillo@gmail.com>
2024-12-08 14:16:51 -0500

    creacion de registros

--- ProyectoP2/Vendedores.xaml.cs
+++ ProyectoP2/Vendedores.xaml.cs
@@ -1,9 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
 namespace ProyectoP2;
 
 public partial class Vendedores : ContentPage
 {
-	public Vendedores()
-	{
-		InitializeComponent();
-	}
+    public Vendedores()
+    {
+        InitializeComponent();
+    }
+
+    private async void LoggingBTN_Clicked(object sender, EventArgs e)
+    {
+        string username = usernameEntry.Text;
+        string password = passwordEntry.Text;
+
+        // Validación de datos
+        if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
+        {
+            await DisplayAlert("Registro exitoso", "Se ha registrado correctamente.", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Registro fallido", "Por favor, complete todos los campos.", "OK");
+        }
+    }
 }
